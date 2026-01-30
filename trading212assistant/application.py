7d2ee0fd7a2ce15d4c6ea6f7645ffcfe460d065d
jwalters <jwalters@stanborough.herts.sch.uk>
2026-01-30 15:29:16 +0000

    Refactor to define constants for cleaner window title initialization

--- trading212assistant/application.py
+++ trading212assistant/application.py
@@ -4,9 +4,13 @@ from sys import argv, exit as sys_exit
 from PySide6.QtWidgets import QApplication, QMainWindow
 
 
+APPLICATION_DISPLAY_NAME: str = 'Trading 212 Assistant'
+APPLICATION_VERSION: str = version('trading-212-assistant')
+
+
 def main() -> None:
     application: QApplication = QApplication(argv)
     main_window: QMainWindow = QMainWindow()
-    main_window.setWindowTitle(f"Trading 212 Assistant {version('trading-212-assistant')}")
+    main_window.setWindowTitle(f"{APPLICATION_DISPLAY_NAME} {APPLICATION_VERSION}")
     main_window.show()
     sys_exit(application.exec())
