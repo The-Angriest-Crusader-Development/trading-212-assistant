38605d1b91d7cd7d8220290f18fe1091587eb58e
jwalters <jwalters@stanborough.herts.sch.uk>
2026-02-02 15:10:53 +0000

    Update `NavigationButtons` to fix height and refine layout handling

--- trading212assistant/ui/navigation_bar/navigation_buttons.py
+++ trading212assistant/ui/navigation_bar/navigation_buttons.py
@@ -12,11 +12,14 @@ class NavigationButtons(QScrollArea):
                  parent: QWidget | None = None) -> None:
         super().__init__(parent)
 
+        self.setFixedHeight(self.sizeHint().height())
         self.setWidgetResizable(True)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
 
         self._container: QWidget = QWidget()
         self._layout: QHBoxLayout = QHBoxLayout(self._container)
+        self._layout.setContentsMargins(0, 0, 0, 0)
+        self._layout.setSpacing(0)
 
         # These are just placeholder buttons
         self._buttons: list[QPushButton] = []
@@ -26,3 +29,7 @@ class NavigationButtons(QScrollArea):
             self._buttons.append(button)
 
         self.setWidget(self._container)
+
+        container_height = self._container.sizeHint().height()
+        horizontal_scrollbar_height = self.horizontalScrollBar().sizeHint().height()
+        self.setFixedHeight(container_height + horizontal_scrollbar_height + self.frameWidth() * 2)
