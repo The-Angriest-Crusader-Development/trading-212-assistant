40eca0c5205a7c004ff4c381f64d8fd018a302d1
jwalters <jwalters@stanborough.herts.sch.uk>
2026-02-02 12:14:36 +0000

    Enhance `MainWindow` layout by adding `QStackedWidget` for main content and navigation bar with `QHBoxLayout`

--- trading212assistant/ui/main_window.py
+++ trading212assistant/ui/main_window.py
@@ -1,5 +1,7 @@
 from PySide6.QtWidgets import (
+    QHBoxLayout,
     QMainWindow,
+    QStackedWidget,
     QVBoxLayout,
     QWidget
 )
@@ -9,12 +11,19 @@ class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
 
-        central_widget: QWidget = QWidget()
-        self.setCentralWidget(central_widget)
+        self._central_widget: QWidget = QWidget()
+        self.setCentralWidget(self._central_widget)
 
-        self._central_layout: QVBoxLayout = QVBoxLayout(central_widget)
+        self._central_layout: QVBoxLayout = QVBoxLayout(self._central_widget)
 
         self._build_ui()
 
     def _build_ui(self):
-        pass
+        # TODO: Replace these implementations of the main content and navigation bar with QWidget subclasses for better
+        #  encapsulation.
+        self.main_content: QStackedWidget = QStackedWidget(self._central_widget)
+        self._central_layout.addWidget(self.main_content)
+
+        self._navigation_bar_widget: QWidget = QWidget(self._central_widget)
+        self._navigation_bar_layout: QHBoxLayout = QHBoxLayout(self._navigation_bar_widget)
+        self._central_layout.addWidget(self._navigation_bar_widget)
