2273ea460db4f82d9dc4ed9f30b96649ea54c8f4
jwalters <jwalters@stanborough.herts.sch.uk>
2026-02-03 10:11:40 +0000

    Update `MainContent` to accept an optional parent widget

--- trading212assistant/ui/main_content/main_content.py
+++ trading212assistant/ui/main_content/main_content.py
@@ -1,6 +1,7 @@
-from PySide6.QtWidgets import QStackedWidget
+from PySide6.QtWidgets import QStackedWidget, QWidget
 
 
 class MainContent(QStackedWidget):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self,
+                 parent: QWidget | None = None) -> None:
+        super().__init__(parent)
