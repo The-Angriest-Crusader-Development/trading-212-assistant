4c7d061efa66ff05e4ea41c3848e000422349c4e
jwalters <jwalters@stanborough.herts.sch.uk>
2026-02-03 11:40:37 +0000

    Add page registration and navigation methods to `MainContent`

--- trading212assistant/ui/main_content/main_content.py
+++ trading212assistant/ui/main_content/main_content.py
@@ -5,3 +5,18 @@ class MainContent(QStackedWidget):
     def __init__(self,
                  parent: QWidget | None = None) -> None:
         super().__init__(parent)
+        self._page_registry: dict[str, QWidget] = {}
+
+    def _register_page(self,
+                       key: str,
+                       widget: QWidget) -> None:
+        self.addWidget(widget)
+        self._page_registry[key] = widget
+
+    def show_page(self,
+                  page_key: str) -> None:
+        widget: QWidget = self._page_registry[page_key]
+        index: int = self.indexOf(widget)
+
+        if index == -1:
+            self.setCurrentIndex(index)
