f3f9e76ccceebf31cc46d75b4a933a845fd9b256
jwalters <jwalters@stanborough.herts.sch.uk>
2026-02-03 13:49:39 +0000

    Add initial implementations for `HomePage`, `SAndSIsaPage`, and `CfdPage`, and update `PAGE_DEFINITIONS` to use them

--- trading212assistant/ui/pages/page_definitions.py
+++ trading212assistant/ui/pages/page_definitions.py
@@ -3,6 +3,10 @@ from dataclasses import dataclass
 
 from PySide6.QtWidgets import QWidget
 
+from .cfd_page import CfdPage
+from .home_page import HomePage
+from .s_and_s_isa_page import SAndSIsaPage
+
 
 @dataclass(frozen=True)
 class PageDefinition:
@@ -13,7 +17,7 @@ class PageDefinition:
 
 # TODO: Create real pages for these PageDefinition factories.
 PAGE_DEFINITIONS: Sequence[PageDefinition] = (
-    PageDefinition(display_name='Home', factory=QWidget, key='home'),
-    PageDefinition(display_name='S&S ISA', factory=QWidget, key='s&s isa'),
-    PageDefinition(display_name='CFD', factory=QWidget, key='cfd')
+    PageDefinition(display_name='Home', factory=HomePage, key='home'),
+    PageDefinition(display_name='S&S ISA', factory=SAndSIsaPage, key='s and s isa'),
+    PageDefinition(display_name='CFD', factory=CfdPage, key='cfd')
 )
---
+++ trading212assistant/ui/pages/s_and_s_isa_page.py
@@ -0,0 +1,11 @@
+from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget
+
+
+class SAndSIsaPage(QWidget):
+    def __init__(
+        self,
+        parent: QWidget | None = None
+    ) -> None:
+        super().__init__(parent)
+        self._layout = QVBoxLayout(self)
+        self._layout.addWidget(QLabel('S&S ISA'))
